605136ab48681076c0ed9568de8bb01290293a99
joctan-maceda <123424180+joctan-maceda@users.noreply.github.com>
2024-09-26 14:31:42 -0600

    Agregamos los ejercicios hasta el 8

--- Actividades/src/main.js
+++ Actividades/src/main.js
@@ -85,3 +85,60 @@ function SentenciaIfElse(){
     document.write('el mayor es '+num2);
     }
 }
+
+function SentenciaIfElseAnidada(){
+    var nota1,nota2,nota3;
+
+    nota1 = prompt('Ingresa 1ra. nota:', '');
+    nota2 = prompt('Ingresa 2da. nota:', '');
+    nota3 = prompt('Ingresa 3ra. nota:', '');
+
+    //Convertimos los 3 string en enteros
+    nota1 = parseInt(nota1);
+    nota2 = parseInt(nota2);
+    nota3 = parseInt(nota3);
+
+    var pro;
+    pro = (nota1+nota2+nota3)/3;
+    if (pro>=7) {
+        document.write('aprobado');
+        }
+        else {
+        if (pro>=4) {
+        document.write('regular');
+        }
+        else {
+        document.write('reprobado');
+        }
+        }
+}
+
+function Sentenciaswitch(){
+    var valor;
+    valor = prompt('Ingresar un valor comprendido entre 1 y 5:', '' );
+    //Convertimos a entero
+    valor = parseInt(valor);
+    switch (valor) {
+    case 1: document.write('uno');
+
+    break;
+
+    case 2: document.write('dos');
+
+    break;
+
+    case 3: document.write('tres');
+
+    break;
+
+    case 4: document.write('cuatro');
+
+    break;
+
+    case 5: document.write('cinco');
+
+    break;
+
+    default:document.write('debe ingresar un valor comprendido entre 1 y 5.');
+    }
+}
