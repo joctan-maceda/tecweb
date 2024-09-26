34a97880e36be6242440ce90b17b72b13da909c3
joctan-maceda <123424180+joctan-maceda@users.noreply.github.com>
2024-09-26 14:22:20 -0600

    Agregamos ejercicio 2 y 4 del mismo archivo

--- Actividades/src/main.js
+++ Actividades/src/main.js
@@ -34,3 +34,30 @@ function variables(){
     document.write( '<br>' );
     document.write( casado );
 }
+
+
+function EntradaDatos(){
+    var nombre;
+    var edad;
+    nombre = prompt('Ingresa tu nombre:', '');
+    edad = prompt('Ingresa tu edad:', '');
+    document.write('Hola ');
+    document.write(nombre);
+    document.write(' así que tienes ');
+    document.write(edad);
+    document.write(' años');
+}
+
+function SumayProducto(){
+    var valor1;
+    var valor2;
+    valor1 = prompt('Introducir primer número:', '');
+    valor2 = prompt('Introducir segundo número', '');
+    var suma = parseInt(valor1)+parseInt(valor2);
+    var producto = parseInt(valor1)*parseInt(valor2);
+    document.write('La suma es ');
+    document.write(suma);
+    document.write('<br>');
+    document.write('El producto es ');
+    document.write(producto);
+}
