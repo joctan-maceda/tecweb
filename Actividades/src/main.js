dbc62bdcd32c66f6eeae7631f165cbce8ce2e71f
joctan-maceda <123424180+joctan-maceda@users.noreply.github.com>
2024-09-26 14:48:46 -0600

    Agregamos los ejercicios hasta el 18

--- Actividades/src/main.js
+++ Actividades/src/main.js
@@ -246,3 +246,45 @@ function Funciones3(){
         valor2 = parseInt(valor2);
         mostrarRango(valor1,valor2);
 }
+
+function Retornovalor(){
+    function convertirCastellano(x) {
+
+        if(x==1)
+        return 'uno';
+        else
+        if(x==2)
+        
+        return 'dos';
+        else
+        if(x==3)
+        return 'tres';
+        else
+        if(x==4)
+        
+        return 'cuatro';
+        
+        else
+        
+        if(x==5)
+        return 'cinco';
+        else
+        return 'valor incorrecto';
+        
+        }
+        var valor = prompt('Ingresa un valor entre 1 y 5', '');
+        valor = parseInt(valor);
+        var r = convertirCastellano(valor);
+        document.write(r);
+}
+
+function Retornovalor2(){
+    switch (x) {
+        case 1: return "uno";
+        case 2: return "dos";
+        case 3: return "tres";
+        case 4: return "cuatro";
+        case 5: return "cinco";
+        default: return "valor incorrecto";
+        }
+}
