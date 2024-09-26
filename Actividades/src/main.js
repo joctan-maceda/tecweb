474f913cc0e539a63f21ab369ef90024f6d528a3
joctan-maceda <123424180+joctan-maceda@users.noreply.github.com>
2024-09-26 14:15:33 -0600

    Agregamos una carpeta nueva de actividades y los primeros dos ejercicios

---
+++ Actividades/src/main.js
@@ -0,0 +1,36 @@
+
+function getDatos()
+{
+    var nombre = prompt("Nombre: ", "");
+
+    var edad = prompt("Edad: ", 0);
+
+    var div1 = document.getElementById('nombre');
+    div1.innerHTML = '<h3> Nombre: '+nombre+'</h3>';
+
+    var div2 = document.getElementById('edad');
+    div2.innerHTML = '<h3> Edad: '+edad+'</h3>';
+}
+
+function HolaMundo()
+{
+    document.write('Hola Mundo');
+}
+
+function variables(){
+    var nombre = 'Juan';
+    var edad = 10
+    ;
+    var altura = 1.92
+    ;
+    var casado = false
+    ;
+
+    document.write( nombre );
+    document.write( '<br>' );
+    document.write( edad );
+    document.write( '<br>' );
+    document.write( altura );
+    document.write( '<br>' );
+    document.write( casado );
+}
