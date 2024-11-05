8d41bd6e1e6a1519c0822ad042521bd2306a0af4
joctan-maceda <123424180+joctan-maceda@users.noreply.github.com>
2024-11-05 14:23:27 -0600

    Agregamos los ejemplos faltantes

--- Ejemplos/PHP/02-atributos_metodos/Menu.php
+++ Ejemplos/PHP/02-atributos_metodos/Menu.php
@@ -1,23 +1,21 @@
 <?php
-    class Menu{
-        private $enlaces = array();
-        private $titulos = array();
-    
+class Menu {
+    private $enlaces = array();
+    private $titulos = array();
 
-        public function cargar_opcion($link, $title){
-            $this->enlaces[] = $link;
-            $this->titulos[] = $title;
-            
-        }
+    public function cargar_opcion($link, $title) {
+        $this->enlaces[] = $link;
+        $this->titulos[] = $title;
+    }
 
-        public function mostrar(){
-            for($i=0; $i<count($this->enlaces); $i++){
-                echo '<a href=" '.$this->enlaces[$i].'">'.$this->titulos[$i].'</a>';
+    public function mostrar() {
+        for($i=0; $i<count($this->enlaces); $i++) {
+            echo '<a href="'.$this->enlaces[$i].'">'.$this->titulos[$i].'</a>';
 
-                if($i < count($this->enlaces)-1){
-                    echo ' - ';
-                }
+            if($i<count($this->enlaces)-1) {
+                echo '-';
             }
         }
     }
+}
 ?>
--- Ejemplos/PHP/02-atributos_metodos/index.php
+++ Ejemplos/PHP/02-atributos_metodos/index.php
@@ -2,18 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
     <?php
-        require_once __DIR__. '/Menu.php';
+        require_once __DIR__ . '/Menu.php';
 
         $menu1 = new Menu;
-        $menu1->cargar_opcion('/facebook.com', 'Facebook');
-        $menu1->cargar_opcion('/twiter.com', 'Twitter');
-        $menu1->cargar_opcion('/instagram.com', 'Instagram');
+        $menu1->cargar_opcion('https://www.facebook.com/', 'Facebook');
+        $menu1->cargar_opcion('https://twitter.com/', 'Twitter');
+        $menu1->cargar_opcion('https://www.instagram.com/', 'Instagram');
         $menu1->mostrar();
     ?>
+
 </body>
 </html>
